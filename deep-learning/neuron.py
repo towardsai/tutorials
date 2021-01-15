dcb1579c91dce566742ccc3b20759f7cf625515d
Roberto <contact@robertoiriondo.com>
2021-01-14 22:59:16 -0500

    added deep learning placeholder

---
+++ deep-learning/neuron.py
@@ -0,0 +1,27 @@
+# import numpy
+import numpy as np
+
+# define our sigmoid function
+def sigmoid(x):
+  return 1/ (1 + np.exp(-x))
+
+# craete the AN
+class Neuron:
+  def __init__(self, weights, bias):
+    self.weights = weights
+    self.bias = bias
+
+  def feedforwards(self, inputs):
+    total = np.dot(self.weights, inputs) + self.bias
+    return sigmoid(total)
+
+weights = np.array([0, 1])
+bias = 4
+
+neuron = Neuron(weights, bias)
+
+x = np.array([2, 3])
+
+forward = neuron.feedforwards(x)
+
+print(forward)
